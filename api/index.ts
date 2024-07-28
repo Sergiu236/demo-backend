75d145c01ff82e6164e2518c147cc391b8feaa6d
Sergiu236 <sergiumocan04@yahoo.com>
2024-07-28 11:40:10 +0300

    refactor accept moca

--- api/index.ts
+++ api/index.ts
@@ -4,6 +4,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const cors = require('cors');
+// Enable CORS for all domains
+app.use(cors());
+
 app.get('/api/hello', async (req: Request, res: Response) => {
   return res.status(200).json({ message: 'Hello World' });
 });
@@ -11,8 +15,7 @@ app.get('/api/hello', async (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+
 module.exports = app;
 
-const cors = require('cors');const cors = require('cors');
-// Enable CORS for all domains
-app.use(cors());
