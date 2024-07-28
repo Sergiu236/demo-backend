8897e5fa1e4109a67a521c5c17689a9ee0a06862
Sergiu236 <sergiumocan04@yahoo.com>
2024-07-28 11:17:18 +0300

    refactor import

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-import { Request, Response } from 'express';
+const { Request, Response } = require('express');
+const express = require('express');
 
 const app = express();
 const port = 3000;
@@ -11,5 +11,4 @@ app.get('/api/hello', async (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
-export default app;
+module.exports = app;
