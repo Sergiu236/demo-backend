e4527f766c5b9c0c621dd3e315a6d3fb34d9afce
Sergiu236 <sergiumocan04@yahoo.com>
2024-08-23 22:29:20 +0300

    update firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -12,13 +12,13 @@ const serviceAccount = {
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
     "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-qucwa%40fir-hotel-2aebe.iam.gserviceaccount.com",
     "universe_domain": "googleapis.com",
-    "databaseURL": "https://fir-hotel-2aebe-default-rtdb.europe-west1.firebasedatabase.app",
-    "storageBucket": "gs://fir-hotel-2aebe.appspot.com"
   }
   
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://fir-hotel-2aebe-default-rtdb.europe-west1.firebasedatabase.app",
+  storageBucket: "gs://fir-hotel-2aebe.appspot.com"
   
 });
 const db = admin.database();
